2a6a1c6d9151e47b976c04110fc38c750f44e98c
Mohammad Imran <imranmbhd2412@gmail.com>
2025-04-15 19:45:19 +0530

    feat: uncomment ProductDetails component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { React } from "react";
 
 import YoutubeButtons from "./components/YoutubeButtons";
-import ProductDetails from "./components/ProductDetails";
+// import ProductDetails from "./components/ProductDetails";
 function App() {
   return (
     <>
       <YoutubeButtons />
-      <ProductDetails />
+      {/* <ProductDetails /> */}
     </>
   );
 }
